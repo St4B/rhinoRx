25cada9f53091ed1e5bd2edeb1323e10249ff184
St4B <stabthegosu@hotmail.com>
2018-10-05 14:02:12 +0300

    Remove not needed fields in scripting result

--- app/src/main/java/quadible/com/js/ScriptingResult.java
+++ app/src/main/java/quadible/com/js/ScriptingResult.java
@@ -9,32 +9,18 @@ public final class ScriptingResult {
 
     private final Map<String, String> mValues;
 
-    private final ArrayList<String> mAlerts;
-
-    private final ArrayList<String> mDialogs;
-
     private final boolean applyValues;
 
     private final boolean isDebugEnabled;
 
     private ScriptingResult(Builder builder) {
         mValues =  Collections.unmodifiableMap(builder.mValues);
-        mAlerts = builder.mAlerts;
-        mDialogs = builder.mDialogs;
         applyValues = builder.applyValues;
         isDebugEnabled = builder.isDebugEnabled;
     }
 
     public Map<String, String> getValues() {
         return mValues;
-    }
-
-    public ArrayList<String> getAlerts() {
-        return mAlerts;
-    }
-
-    public ArrayList<String> getDialogs() {
-        return mDialogs;
     }
 
     public boolean isApplyValues() {
@@ -59,16 +45,6 @@ public final class ScriptingResult {
 
         public Builder values(HashMap<String, String> values) {
             mValues = values;
-            return this;
-        }
-
-        public Builder alert(String msg) {
-            mAlerts.add(msg);
-            return this;
-        }
-
-        public Builder dialog(String msg) {
-            mDialogs.add(msg);
             return this;
         }
 
